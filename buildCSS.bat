a4e74483f0b7807d6bf7cf4e4e641f57db40d540
VBaykin <vbaykin@mail.ru>
2019-02-19 22:23:46 +0400

    справка в ИФНС

--- buildCSS.bat
+++ buildCSS.bat
@@ -3,3 +3,4 @@ copy chip_mo.css d:\_mo\chip\exe
 copy chip_mo.js d:\_mo\chip\exe
 copy jquery-3.2.1.min.js d:\_mo\chip\exe
 copy _dogovor.shb d:\_mo\chip\exe
+copy ReferenceToFTS.shb d:\_mo\chip\exe
