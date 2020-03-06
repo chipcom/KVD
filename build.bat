9d6d6de1ffee9a3f5e62f0872858a3b285c42593
chipcom <vbaykin@mail.ru>
2020-03-06 12:15:35 +0400

    добавил талон пациента, изменил список для архива

--- build.bat
+++ build.bat
@@ -4,6 +4,7 @@ copy chip.css d:\_mo\chip\exe
 copy chip_mo.css d:\_mo\chip\exe
 copy chip_mo.js d:\_mo\chip\exe
 copy _dogovor.shb d:\_mo\chip\exe
+copy list_uch.shb d:\_mo\chip\exe
 copy ReferenceToFTS.shb d:\_mo\chip\exe
 rem copy jquery-3.2.1.min.js d:\_mo\chip\exe
 rar a ..\source\chip_mo.rar @my_chip_mo.lst
