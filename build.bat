cf16b071d241cc98fe286d310be46ee1da0703c7
VBaykin <vbaykin@mail.ru>
2021-01-25 19:43:57 +0300

    edit bat-file

--- build.bat
+++ build.bat
@@ -1,9 +1,9 @@
 c:\Harbour\bin\hbmk2 chip_mo_bay.hbp
-copy chip_mo.exe d:\_mo\chip\exe
-copy chip.css d:\_mo\chip\exe
-copy chip_mo.css d:\_mo\chip\exe
-copy chip_mo.js d:\_mo\chip\exe
-copy _dogovor.shb d:\_mo\chip\exe
-copy list_uch.shb d:\_mo\chip\exe
-copy ReferenceToFTS.shb d:\_mo\chip\exe
-rar a ..\build\KVD\chip_mo.rar @my_chip_mo.lst
+@REM copy chip_mo.exe d:\_mo\chip\exe
+@REM copy chip.css d:\_mo\chip\exe
+@REM copy chip_mo.css d:\_mo\chip\exe
+@REM copy chip_mo.js d:\_mo\chip\exe
+@REM copy _dogovor.shb d:\_mo\chip\exe
+@REM copy list_uch.shb d:\_mo\chip\exe
+@REM copy ReferenceToFTS.shb d:\_mo\chip\exe
+@REM rar a ..\build\KVD\chip_mo.rar @my_chip_mo.lst
