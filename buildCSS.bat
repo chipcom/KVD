c81aa4c9faf334106f7c143d2f68dc46c458f49c
chipcom <vbaykin@mail.ru>
2025-01-17 11:00:23 +0300

    удаление справки ФНС

--- buildCSS.bat
+++ buildCSS.bat
@@ -2,4 +2,4 @@ copy chip.css d:\_mo\chip\exe
 copy chip_mo.css d:\_mo\chip\exe
 copy chip_mo.js d:\_mo\chip\exe
 copy _dogovor.shb d:\_mo\chip\exe
-copy ReferenceToFTS.shb d:\_mo\chip\exe
+rem copy ReferenceToFTS.shb d:\_mo\chip\exe
--- build_dev.bat
+++ build_dev.bat
@@ -5,5 +5,5 @@ copy chip_mo.css d:\_mo\chip\exe
 copy chip_mo.js d:\_mo\chip\exe
 copy _dogovor.shb d:\_mo\chip\exe
 copy list_uch.shb d:\_mo\chip\exe
-copy ReferenceToFTS.shb d:\_mo\chip\exe
+rem copy ReferenceToFTS.shb d:\_mo\chip\exe
 rem rar a -ep d:\_mo\_build\KVD\chip_mo @my_chip_mo.lst
